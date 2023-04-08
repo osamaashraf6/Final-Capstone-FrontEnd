134e70e93cae3394c3cc8608ceb265d1a7e15e33
osamaashraf6 <osamaashraf668@gmail.com>
2023-04-08 20:33:14 +0200

    Add the AddReserve page at the navigation

---
+++ src/components/AddReserve/AddReserve.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+function AddReserve() {
+  return (
+    <>
+      <div className="">AllProducts AddReserve</div>
+    </>
+  );
+}
+
+export default AddReserve;
