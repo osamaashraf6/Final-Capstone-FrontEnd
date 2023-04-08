e8881bfc331450457d52e7cd9a1aea0fbd7064a6
osamaashraf6 <osamaashraf668@gmail.com>
2023-04-08 20:32:34 +0200

    Add the Reserve page at the navigation

---
+++ src/components/Reserve/Reserve.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+function Reservee() {
+  return (
+    <>
+      <div className="">
+        Reservee
+      </div>
+    </>
+  );
+}
+
+export default Reservee;
