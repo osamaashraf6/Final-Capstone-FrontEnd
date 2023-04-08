83ace49e97c31b28f58fda0a7235edc06cf9d2b9
osamaashraf6 <osamaashraf668@gmail.com>
2023-04-08 20:35:02 +0200

    Add the pages folder for the routes of the project

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import HomeList from '../components/Home/HomeList';
+
+function Home() {
+  return (
+    <>
+      <h4 className="text-center mb-4">Welcome to Swimming Class</h4>
+      <HomeList />
+    </>
+  );
+}
+
+export default Home;
