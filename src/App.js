3e8696019c55fb2bd096f5d7f1d9f4b2089b9fdb
osamaashraf6 <osamaashraf668@gmail.com>
2023-04-08 20:35:26 +0200

    Fix the linter error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import Authentication from './components/authentication';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar.js';
+import Home from './pages/Home.js';
+import Reserve from './pages/Reserve.js';
+import AddReserve from './pages/AddReserve.js';
+import SignUp from './pages/SignOut.js';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SignOut from './pages/SignOut.js';
 
 function App() {
   return (
     <>
-      <h4 className="text-center mb-4">Welcome to Swimming Class</h4>
-      <Authentication />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/*  */}
+          <Route index element={<Home />} />
+          <Route path="Home" element={<Home />} />
+          {/*  */}
+          <Route path="Reserve" element={<Reserve />} />
+          {/*  */}
+          <Route path="SignUp" element={<SignOut />} />
+          {/*  */}
+          <Route path="AddReserve" element={<AddReserve />} />
+          {/*  */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
